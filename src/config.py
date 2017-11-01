845a3b7fa896d0436cea2e08e33a7c9f1300ee85
wangyuqing <wangyuqing@qiyi.com>
2017-11-02 00:20:05 +0800

    @update manage.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ HOSTNAME = '127.0.0.1'
 PORT     = '3306'
 DATABASE = 'babyschool'
 USERNAME = 'root'
-PASSWORD = 'mysqlmima'
+PASSWORD = '1'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
--- src/manage.py
+++ src/manage.py
@@ -9,22 +9,24 @@ from app.auth import auth
 from app.basehome import home
 from flask import Flask
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
-from models import UserModel
 from exts import db
 
 app = Flask(__name__)
 app.config.from_object(config)
-db.init_app(app)
+migrate = Migrate(app, db)
 
+from models import UserModel
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 app.register_blueprint(home, url_prefix='')
 app.register_blueprint(home)
 app.register_blueprint(auth, url_prefix='')
 app.register_blueprint(auth)
-
-manager = Manager(app)
 
 if __name__ == '__main__':
     # for i in app.url_map.iter_rules():
