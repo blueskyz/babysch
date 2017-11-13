c02f50700475cd687ac1eee1cc52fa572453f319
郝龙 <haolongost@163.com>
2017-11-13 15:35:00 +0800

    添加 预约信息管理

--- src/app/schedulecourse/views.py
+++ src/app/schedulecourse/views.py
@@ -6,26 +6,8 @@ import flask
 from . import course
 from forms import ScheduleForm
 from models import ScheduleRecordModel
-from flask_login import current_user
+from flask_login import current_user,login_required
 from exts import db
-
-
-'''
-form = AuthForm()
-
-    # 验证手机号密码
-    if form.validate_on_submit():
-        phone = request.form.get('phone')
-        passwd = request.form.get('passwd')
-        user_info = User.query.filter_by(telephone=phone).first()
-        if user_info and user_info.check_password(passwd):
-            login_user(user_info)
-            return redirect('/')
-        else:
-            flash('用户名密码不匹配')
-
-    return render_template('auth/login.html', form=form)
-'''
 
 
 @course.route('/schedule/',methods = ['GET','POST'])
@@ -48,7 +30,7 @@ def schedule():
             if record:
                 return redirect("/")
             else:
-                model = ScheduleRecordModel(phone=phone,childage=1,childname='xiaohong')
+                model = ScheduleRecordModel(phone=phone,childage=age,childname=name)
                 db.session.add(model)
                 db.session.commit()
                 return redirect("/")
@@ -59,13 +41,17 @@ def schedule():
 
 
 @course.route('/schedule-list/',methods=['GET'])
+@login_required
 def schedule_list():
     """
     预约列表
     """
+    print('页面错误')
     if not current_user.is_admin():
+        print('页面错误')
         return 'No Access', 403
-    schedule_list = ScheduleRecordModel.query.all()
-    return render_template('schedul_list.html',schedule_list = schedule_list)
+    else:
+        schedule_list = ScheduleRecordModel.query.all()
+        return render_template('schedul_list.html',schedule_list = schedule_list)
 
 
